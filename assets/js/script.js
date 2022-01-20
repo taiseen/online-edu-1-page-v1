dc6aced95b58bc57ee99059ea7eb282b6664df1c
taiseen <taiseen.cse@gmail.com>
2022-01-20 11:24:56 +0600

    till about ==> OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,9 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('userClick');
 });
+
+
+window.addEventListener('scroll', () => {
+    menu.classList.remove('fa-times');
+    header.classList.remove('userClick');
+});
